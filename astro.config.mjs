560e6de70d7fcf079486f64638511551c6d7bf5d
Fumie Toyooka <dymojet@gmail.com>
2022-12-18 23:45:24 +0900

    :bug:

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,7 +12,7 @@ import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://sunny-arithmetic-1e866e.netlify.app",
+  site: "https://fumicoswork.netlify.app",
   experimental: {
     integrations: true
   },

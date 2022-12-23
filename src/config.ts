09195073a87a745160a807a6732915d0e6011138
Fumie Toyooka <dymojet@gmail.com>
2022-12-23 13:11:14 +0900

    :sparkles:

--- src/config.ts
+++ src/config.ts
@@ -2,6 +2,6 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "fumicos";
-export const SITE_SUB_TITLE = "Welcome to fumicos blog!";
-export const SITE_DESCRIPTION = "Welcome to fumicos\' blog!";
+export const SITE_SUB_TITLE = "Welcome to my blog!";
+export const SITE_DESCRIPTION = "Blog and more";
 export const SITE_URL = "https://fumicos.work";
